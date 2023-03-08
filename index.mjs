a96678c095ebe8c294b8af94684e91fe5cadc8c9
huzaifahux <www.pakteach@gmail.com>
2023-03-09 00:00:59 +0500

    product add and get api ready

--- index.mjs
+++ index.mjs
@@ -2,26 +2,37 @@ import express from "express"
 const app = express()
 const port = 3000;
 import cors from "cors"
+app.use(express.json());
 app.use(cors())
+
+const database = []
+
 app.get('/', (req, res) => {
     console.log(new Date().toString())
     res.send('Hello World!')
 
 })
-app.get('/weather', (req, res) => {
-    res.send(
-        {
-            temp: 30,
-            humadity: 431.2,
-        });
-    console.log("our Response", res)
+app.post('/addproduct', (req, res) => {
+    const { name, price } = req.body
+    console.log("product :", req.body)
+    res.send(`Done, added product ${name}`);
+    database.push({
+        productName: name,
+        productPrice: price
+    })
+    console.log("Data From DB", database);
+
 })
+ app.get('/products', (req,res)=>{
+    res.send(database)
+
+ })
 
 app.use('/page', express.static('./web'))
 
 app.get('/contact', (req, res) => {
     res.send(`<h1>It's Contact Section</h1>`)
-
+    
 })
 
 
